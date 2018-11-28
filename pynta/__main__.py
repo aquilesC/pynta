f740e01c32ff7d91efc5e90e652f55cd9099c20b
Aquiles Carattino <aquiles@aquicarattino.com>
2018-11-28 16:27:59 +0100

    Entry point for pynta

--- pynta/__main__.py
+++ pynta/__main__.py
@@ -1,3 +1,41 @@
-from pynta import __version__
+import logging
+import sys
+import traceback
 
-print('PyNTA version: {}'.format(__version__))
+from PyQt5.QtWidgets import QApplication
+
+from pynta.model.experiment.nano_cet.win_nanocet import NanoCET
+from pynta.util.log import get_logger
+from pynta.view.mainwindow import MainWindow
+
+
+logger = get_logger(name='pynta.model.experiment.publisher')  # 'pynta.model.experiment.nano_cet.saver'
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser(description='Start the pyNTA software')
+    parser.add_argument("-c", dest="config_file", required=True,
+                        help="Path to the configuration file")
+    args = parser.parse_args()
+
+    with NanoCET(args.config_file) as exp:
+        exp.initialize_camera()
+
+        try:
+            app = QApplication([])
+            window = MainWindow(exp)
+            window.show()
+            app.exec_()
+        except Exception as e:
+            print('An exception on the GUI appeared.')
+            print(e)
+            traceback.print_exc(file=sys.stdout)
+
